a5f2cab064d96d2f1a2bb90504c9ec0bc6600aa7
NafisaTabassum01 <tabassumnafisa28.01@gmail.com>
2026-04-30 17:03:01 +0600

    animals details added

--- src/app/(main)/animal/[id]/page.jsx
+++ src/app/(main)/animal/[id]/page.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import Image from 'next/image';
+import cow4 from '../../../../assets/cow-4.png';
+import cow1 from '../../../../assets/cow.png';
+import cow2 from '../../../../assets/cow-2.png';
+import goat from '../../../../assets/goat.png';
+import camel from '../../../../assets/camel.png';
+import cow3 from '../../../../assets/cow-3.png';
+import cow5 from '../../../../assets/cow-5.png';
+import cow6 from '../../../../assets/cow-6.png';
+import cow7 from '../../../../assets/cow-7.png';
+import goat2 from '../../../../assets/goat-2.png';
+import goat3 from '../../../../assets/goat-3.png';
+import goat4 from '../../../../assets/goat-4.png';
 
-const page = () => {
-    return (
-        <div>
-            animal details
-        </div>
-    );
+
+const imageMap = {
+  "cow.png": cow1,
+  "cow-2.png": cow2,
+  "goat.png": goat,
+  "camel.png": camel,
+  "cow-4.png": cow4,
+  "cow-3.png": cow3,
+  "cow-6.png": cow6,
+  "cow-7.png": cow7,
+  "cow-5.png": cow5,
+  "goat-2.png": goat2,
+  "goat-3.png": goat3,
+  "goat-4.png": goat4,
 };
 
-export default page;
+
+
+export default async function Page({ params }) {
+
+ const { id } = await params;
+ const res = await fetch(
+    "https://gist.githubusercontent.com/NafisaTabassum01/4b3e6993282ad6aa93f91fc3bfab3d88/raw/4acc6e50887dee8c83b804e4dbd6de2c00530d2a/gistfile1.txt",
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+
+  const animal = data.find((item) => item.id === Number(id));
+
+  if (!animal) {
+    return (
+      <div>
+        <h2>Not Found</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-[#fefae0d6] py-10">
+      <div className="w-10/12 mx-auto flex flex-col gap-16 items-center shadow-lg rounded-2xl md:flex-row justify-center bg-[#fffef94b]">
+        <Image src={imageMap[animal.image]} alt='' className='md:h-50 w-90'/>
+      <div>
+     <div>
+         <h1 className="text-2xl font-bold text-[#AE6422] pt-5">{animal.name}</h1>
+      <p className='badge badge-soft text-[14px] bg-transparent border border-[#AA6122] text-[#AA6122] rounded-2xl mt-2'>{animal.category}</p>
+     </div>
+      <p className='text-[24px] pt-1.5 font-bold text-green-900'><i className="fa-solid fa-bangladeshi-taka-sign"></i>{animal.price}</p>
+      <div className='grid grid-cols-2 gap-8 pb-8'>
+        <div>
+        <p className='text-[18px] pt-4 font-medium text-[#BC6C25]'><i className="fa-solid fa-dna"></i> Breed</p>
+        <p className='text-[18px] pt-2 font-medium text-[#BC6C25]'><i className="fa-regular fa-calendar-days"></i> Age</p>
+        <p className='text-[18px] pt-2 font-medium text-[#BC6C25]'><i className="fa-solid fa-weight-hanging"></i> Weight</p>
+        <p className='text-[18px] pt-2 font-medium text-[#BC6C25]'><i className="fa-solid fa-location-dot"></i> Location</p>
+        </div>
+        <div>
+            <p className='text-[18px] pt-4 font-semibold text-[#894E1B]'>{animal.breed}</p>
+            <p className='text-[18px] pt-2 font-semibold text-[#894E1B]'>{animal.age}</p>
+            <p className='text-[18px] pt-2 font-semibold text-[#894E1B]'>{animal.weight} kg</p>
+            <p className='text-[18px] pt-2 font-semibold text-[#894E1B]'>{animal.location}</p>
+        </div>
+      </div>
+      </div>
+      </div>
+      <div className="w-10/12 mx-auto flex flex-col gap-16 items-center shadow-lg rounded-2xl md:flex-row justify-center bg-[#fffef94b] pt-12 mt-12">
+
+      </div>
+    </div>
+  );
+}
